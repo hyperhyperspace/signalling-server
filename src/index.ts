627c15650f2ed08055e33af7bfc73140bb85446e
Santi Bazerque <sbazerque@gmail.com>
2022-10-19 15:04:10 -0300

    added "pong" message

--- src/index.ts
+++ src/index.ts
@@ -39,96 +39,104 @@ wss.on('connection', (ws: WebSocket, request: IncomingMessage) => {
         console.log('received message: ');
         console.log(data);
 
-        const message = JSON.parse(data);
+        try {
+            const message = JSON.parse(data);
 
-        if (message.action === 'pong') {
-            console.log('received pong');
-        } else if (message.action === 'listen') {
-            const linkupId = message.linkupId as string || defaultLinkupId;
-
-            if (linkupId.slice(0, verifiedIdPrefix.length) === verifiedIdPrefix) {
-
-                const identityHash = linkupId.slice(verifiedIdPrefix.length).split('/')[0];
-
-                try {
-                    HashedObject.fromLiteralContextWithValidation(message.idContext).then((id: HashedObject) => {
-                        if (id instanceof Identity && 
-                            Hashing.toHex(id.hash()) === identityHash &&
-                            id.verifySignature(uuid, message.signature || '')) {
-                                
-                                listeners.add(linkupId, ws);
-        
-                                console.log('registering listener for ' + linkupId + ' after id validation');
-                        } else {
-                            console.log('NOT registering listener for ' + linkupId + ': id validation failed');
-                        }
-                    }).catch((reason: any) => {
+            if (message.action === 'pong') {
+                console.log('received pong');
+            } else if (message.action === 'ping') { 
+                ws.send(JSON.stringify({action: 'pong'}));
+            } else if (message.action === 'listen') {
+                const linkupId = message.linkupId as string || defaultLinkupId;
+    
+                if (linkupId.slice(0, verifiedIdPrefix.length) === verifiedIdPrefix) {
+    
+                    const identityHash = linkupId.slice(verifiedIdPrefix.length).split('/')[0];
+    
+                    try {
+                        HashedObject.fromLiteralContextWithValidation(message.idContext).then((id: HashedObject) => {
+                            if (id instanceof Identity && 
+                                Hashing.toHex(id.hash()) === identityHash &&
+                                id.verifySignature(uuid, message.signature || '')) {
+                                    
+                                    listeners.add(linkupId, ws);
+            
+                                    console.log('registering listener for ' + linkupId + ' after id validation');
+                            } else {
+                                console.log('NOT registering listener for ' + linkupId + ': id validation failed');
+                            }
+                        }).catch((reason: any) => {
+                            console.log('ERROR trying to register listener for ' + linkupId + ':');
+                            console.log(reason);
+                        });    
+                    } catch (reason: any) {
                         console.log('ERROR trying to register listener for ' + linkupId + ':');
                         console.log(reason);
-                    });    
-                } catch (reason: any) {
-                    console.log('ERROR trying to register listener for ' + linkupId + ':');
-                    console.log(reason);
-                }
-
-                
-            } else {
-                linkupIds.add(linkupId);
-                listeners.add(linkupId, ws);
+                    }
     
-                console.log('registering listener for ' + linkupId);
-            }
-
-
-            
-        } else if (message.action === 'send') {
-
-            const receiver = message.linkupId || defaultLinkupId;
-            const limit = message.limit;
-
-            console.log('trying to send message to ' + receiver);
-
-            if (listeners.hasKey(receiver)) {
-
-                const toRemove = new Array<WebSocket>();
-                const allWs = Array.from(listeners.get(receiver));
-
-                Shuffle.array(allWs);
-
-                const targetWs = limit === undefined || allWs.length <= limit? allWs : allWs.slice(0, limit);
-
-                for (const wsToTarget of targetWs) {
-                    if (wsToTarget.readyState === wsToTarget.OPEN) {
-                        wsToTarget.send(data);
-                        console.log('found listener for ' + receiver + ', send message');
-                    } else {
-                        toRemove.push(wsToTarget);
+                    
+                } else {
+                    linkupIds.add(linkupId);
+                    listeners.add(linkupId, ws);
+        
+                    console.log('registering listener for ' + linkupId);
+                }
+    
+    
+                
+            } else if (message.action === 'send') {
+    
+                const receiver = message.linkupId || defaultLinkupId;
+                const limit = message.limit;
+    
+                console.log('trying to send message to ' + receiver);
+    
+                if (listeners.hasKey(receiver)) {
+    
+                    const toRemove = new Array<WebSocket>();
+                    const allWs = Array.from(listeners.get(receiver));
+    
+                    Shuffle.array(allWs);
+    
+                    const targetWs = limit === undefined || allWs.length <= limit? allWs : allWs.slice(0, limit);
+    
+                    for (const wsToTarget of targetWs) {
+                        if (wsToTarget.readyState === wsToTarget.OPEN) {
+                            wsToTarget.send(data);
+                            console.log('found listener for ' + receiver + ', send message');
+                        } else {
+                            toRemove.push(wsToTarget);
+                        }
+                    }
+    
+                    for (const wsToRemove of toRemove) {
+                        listeners.delete(receiver, wsToRemove);
+                    }
+    
+                }
+    
+            } else if (message.action === 'query') {
+                const targets = message.linkupIds;
+                const queryId = message.queryId;
+                const hits    = new Array<string>();
+    
+                for (const target of targets) {
+                    for (const listenWs of listeners.get(target)) {
+                        if (listenWs.readyState === listenWs.OPEN) {
+                            hits.push(target);
+                            break;
+                        }
                     }
                 }
-
-                for (const wsToRemove of toRemove) {
-                    listeners.delete(receiver, wsToRemove);
-                }
-
+    
+                console.log('replying to query with id ' + queryId + ' with results: ' + hits);
+                ws.send(JSON.stringify({'action': 'query-reply', 'queryId': queryId, 'hits': hits}));
             }
-
-        } else if (message.action === 'query') {
-            const targets = message.linkupIds;
-            const queryId = message.queryId;
-            const hits    = new Array<string>();
-
-            for (const target of targets) {
-                for (const listenWs of listeners.get(target)) {
-                    if (listenWs.readyState === listenWs.OPEN) {
-                        hits.push(target);
-                        break;
-                    }
-                }
-            }
-
-            console.log('replying to query with id ' + queryId + ' with results: ' + hits);
-            ws.send(JSON.stringify({'action': 'query-reply', 'queryId': queryId, 'hits': hits}));
+        } catch (e) {
+            console.log('error attempting to process message: ', e);
         }
+
+        
     });
 
     ws.send(JSON.stringify({'action': 'update-challenge', 'challenge': uuid}))
